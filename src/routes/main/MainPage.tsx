ebf14e2e5ad939f3893a2a3a62df116ad70a61ea
kim nahyeon <stome6322@inha.edu>
2024-11-28 11:41:06 +0900

    ✨ feat: tailwind css 적용

--- src/routes/main/MainPage.tsx
+++ src/routes/main/MainPage.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type Props = {};
 
 export default function MainPage({}: Props) {
-  return <div>MainPage!!</div>;
+  return <div className="font-bold">MainPage!!</div>;
 }
