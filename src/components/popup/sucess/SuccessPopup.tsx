0d09f5090650acc990b73fe7844190a5b8e14f71
kim nahyeon <stome6322@inha.edu>
2024-12-19 23:55:23 +0900

    design: success popup 디자인 변경

--- src/components/popup/sucess/SuccessPopup.tsx
+++ src/components/popup/sucess/SuccessPopup.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function SuccessPopup({ content }: Props) {
   return (
-    <div className="bg-white z-[100] fixed top-[40%] flex flex-col rounded-2xl justify-center items-center py-10 px-6 gap-4">
+    <div className="max-w-[300px] bg-white z-[101] fixed top-[40%] flex flex-col rounded-2xl justify-center items-center py-10 px-6 gap-4">
       <img src={check} className="max-w-[40%]" />
       <p
         style={{
