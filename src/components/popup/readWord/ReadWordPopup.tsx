75f139676809860a5de4985bc1302b0515c2dbab
kim nahyeon <stome6322@inha.edu>
2024-12-20 14:48:50 +0900

    design: word popup z 인덱스 변경

--- src/components/popup/readWord/ReadWordPopup.tsx
+++ src/components/popup/readWord/ReadWordPopup.tsx
@@ -7,7 +7,7 @@ export default function ReadWordPopup() {
   const buttonText = active.status === false ? "녹화 시작" : "녹화 중단";
 
   return (
-    <div className="bg-white z-[100] fixed top-[40%] flex flex-col rounded-2xl justify-center items-center py-10 px-6 gap-4">
+    <div className="bg-white z-[49] fixed top-[40%] flex flex-col rounded-2xl justify-center items-center py-10 px-6 gap-4">
       <p className="text-[#006FFD] text-6xl font-semibold">엄마</p>
       <p className="text-[gray] text-center text-xl">
         화면에 보이는 단어를
